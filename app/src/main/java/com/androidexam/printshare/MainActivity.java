30d83fa68506a74ada0b1518a36ed598eca8a0d3
enricodelorenzi <42135552+enricodelorenzi@users.noreply.github.com>
2021-01-20 22:32:46 +0100

    modifica1

--- app/src/main/java/com/androidexam/printshare/MainActivity.java
+++ app/src/main/java/com/androidexam/printshare/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        float pluto =2;
         int pippo = 12;
     }
 }
